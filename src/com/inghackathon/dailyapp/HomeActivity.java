51d4f25d4a55c569e9c5d2e74e2796288e14d035
ijemmy <highwind_th@hotmail.com>
2013-11-21 21:30:07 +0100

    HomeActivity 1st version
    All mock-up is ready

--- src/com/inghackathon/dailyapp/HomeActivity.java
+++ src/com/inghackathon/dailyapp/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.inghackathon.dailyapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 //Test from Jemmy
 // test from Fry
 //Test from Alexey
@@ -21,5 +24,22 @@ public class HomeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
+	
+	public void onClickScanBtn(View v){
+		this.toast("Scan Button clicked");
+	}
+	
+	public void onClickCartBtn(View v){
+		this.toast("Cart Button clicked");
+		
+	}
+	
+	public void toast(String s){
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast.makeText(context, s, duration).show();
+	}
+	
 
 }
