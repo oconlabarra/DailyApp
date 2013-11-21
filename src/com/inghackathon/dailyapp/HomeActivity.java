83bc4db2a8b17b01dc7e067800e900c61b962117
metalrockdanclex <metalrockdanclex@mail.ru>
2013-11-21 19:53:47 +0100

    Test from Alexey

--- src/com/inghackathon/dailyapp/HomeActivity.java
+++ src/com/inghackathon/dailyapp/HomeActivity.java
@@ -6,6 +6,9 @@ import android.view.Menu;
 
 public class HomeActivity extends Activity {
 
+	
+	public string YushenkovString ="Test";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
