4b228a91db33ae7fe0082afd9f7423517b816f0c
ijemmy <highwind_th@hotmail.com>
2013-11-21 22:22:38 +0100

    Add Actionbar to home

--- src/com/inghackathon/dailyapp/HomeActivity.java
+++ src/com/inghackathon/dailyapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Toast;
 //Test from Jemmy
@@ -21,8 +22,9 @@ public class HomeActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.home, menu);
+	    return super.onCreateOptionsMenu(menu);
 	}
 	
 	public void onClickScanBtn(View v){
